6738142ffef85d123769c3963688e88fb7c49eb8
swatej <swatejpatil8@gmail,com>
2026-01-07 22:17:34 +0530

    perf: Remove Preloader for faster load time

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,6 @@ export const metadata: Metadata = {
   description: "Find the right MP Government scheme for your business in 60 seconds.",
 };
 
-import Preloader from "@/components/ui/Preloader";
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -29,7 +27,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Preloader />
         {children}
       </body>
     </html>
